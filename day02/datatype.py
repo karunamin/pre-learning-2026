a933834f378a20d7eef9c4e3087ff038d26829fb
karunamin <freedom12528@gmail.com>
2026-01-21 17:26:18 +0900

    사전학습 2일차

---
+++ day02/datatype.py
@@ -0,0 +1,25 @@
+# datatype.py - 자료형 확인
+
+# 정수형
+int_val = 10
+# 실수형
+pi = 3.141592
+# 문자열형
+# 파이썬에서 문자열은 ', " 둘다 사용
+name = 'Hugo' # "Hugo" 와 동일
+# C, C++, C# ,Jave 등은 홑따옴표, 쌍따옴표가 다르게 동작
+# 불형
+is_student = False # True
+# False 0과 동일, True 0 이외
+
+print(int_val)
+print(name + str(int_val)) # int_val은 숫자형이므로 문자열로 합치고자 하면 문자열형으로 바꿔야 함
+
+print(pi)
+print(is_student)
+
+name = '한동현'
+age = 27
+height = 177
+
+print('이름 : ' + name + ', 나이 : ' + str(age) + ", 키 : " + str(height))
